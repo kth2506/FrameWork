73477860fefe00a85572c78a5a5979e19187992d
kim taehwan <ksj4159@naver.com>
2022-07-07 11:44:56 +0900

    // ** FrameWork ver11.0 07.06

--- FrameWork/Main.cpp
+++ FrameWork/Main.cpp
@@ -10,20 +10,20 @@ int main(void)
 {
 
 	system("title 김태환 - 게임");
-
+	
 	system("mode con cols=120 lines=30");
-
+	
 	MainUpdate Main;
 	Main.Initialize();
-
+	
 	ULONGLONG Time = GetTickCount64();
-
+	
 	while (true)
 	{
 		if (Time + 50 < GetTickCount64())
 		{
 			Time = GetTickCount64();
-
+	
 			system("cls");
 			Main.Update();
 			Main.Render();
